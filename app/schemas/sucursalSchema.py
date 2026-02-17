e4879c41c3c7ebfd85b98b10299eb7b3c8da7786
LuxuruMX <lixliu.max@gmail.com>
2026-02-17 10:36:03 -0600

    correciones

--- app/schemas/sucursalSchema.py
+++ app/schemas/sucursalSchema.py
@@ -2,9 +2,9 @@ from pydantic import BaseModel, Field
 
 
 class createSucursal(BaseModel):
-    nombre: str = Field(min_length=5, max_length=20)
-    direccion: str = Field(min_length=5, max_length=100)
-    telefono: int = Field(ge=10000000, le=9999999999)
+    nombre: str = Field(min_length=2, max_length=40)
+    direccion: str = Field(min_length=2, max_length=100)
+    telefono: int
 
 class readSucursal(BaseModel):
     id_suc: int
